eb0700c9382f2e53a8e2ce1d19048689b57badc6
elviiron <150541766+elviiron@users.noreply.github.com>
2024-03-13 14:59:44 +0300

    Create railway_dml.sql

---
+++ scripts/railway_dml.sql
@@ -0,0 +1,3 @@
+INSERT INTO Passenger (passenger_id, last_name, first_name, middle_name, document, information, emil, phone_number) VALUES
+(98, 'Щепина', 'Екатерина', 'Андреевна', 'паспорт', 9222457389, 'vedma@mail.com', 89274783467);
+(99, 'Зотина', 'Вероника', 'Константиновна', 'заграничный паспорт', 723774859, 'ronni@yandex.ru', 89994783404);
